d72fb4700204c3a40a844a65843ef45aab261338
SW1073 <eloiimerino@gmail.com>
2023-05-13 17:27:35 +0200

    api.service created

---
+++ ui/src/app/services/api.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  getRanking() : Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}/ranking`);
+  }
+
+  createUser(id: string) : Observable<any> {
+    return this.http.post<any>(`${environment.apiUrl}/save-user/`, {username: id});
+  }
+
+  getUser(id: string) : Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}/get-user/${id}`);
+  }
+
+}
