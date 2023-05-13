42b96816fadaa886dc17e0427fd26bd0131a043a
jafeth <jafethrafael12@gmail.com>
2023-05-13 15:09:01 +0200

    backend template hecha

--- api/api.js
+++ api/api.js
@@ -8,39 +8,59 @@ const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'pgdb',
+    password: '123',
     port: 5432,
 });
-
 
 router.use(express.json())
 
 // POST user to DB
-router.post("/get-user", (req, res) => {
-    const response = {message: "User saved"};
-    pool.query('INSERT INTO Users (username) VALUES ($1)', [req.body.name], (err, res) => {
+router.post("/save-user", (req, res) => {
+    const username = req.body.username;
+
+    if (username == undefined) {
+        res.status(400).json({ error: 'Bad request' });
+        return;
+    }
+
+    const query = "INSERT INTO Users (username) VALUES ('" + username + "')";
+
+    pool.query(query, (err, query_res) => {
         if(err) {
             console.log(err);
             res.status(500).json({ error: 'Server error' });
         }
-        else res.json(res.rows);
+        else {
+            res.json(query_res.rows);
+        }
     });
-    res.send(response);
 });
 
-// GET user score form DB
+
+
 router.get("/user/:username", (req, res) => {
-    pool.query('SELECT SUM(points) FROM IndividualGame WHERE player = ($1)', (err, res) => {
+    const username = req.params.username;
+
+    if (username == undefined) {
+        res.status(400).json({ error: 'Bad request' });
+        return;
+    }
+
+    let query = "SELECT * FROM IndividualGame WHERE player = '" + username + "'";
+
+    pool.query(query, (err, query_res) => {
         if(err) {
             console.log(err)
             res.status(500).json({ error: 'Server error' });
         }
-        else res.json(res.rows)
+        else res.json(query_res.rows)
     });
 })
 
 // GET ranking fron DB
 router.get("/ranking", (req, res) => {
-     pool.query('SELECT username,SUM(game.points) FROM IndividualGame game JOIN Users u ON u.username=game.playe GROUP BY username ORDER BY SUM(game.points) DESC', (err, res) => { 
+    // TODO fix this
+     pool.query('SELECT username,SUM(game.points) FROM IndividualGame game JOIN Users u ON u.username=game.player GROUP BY username ORDER BY SUM(game.points) DESC', (err, res) => { 
      if(err) {
               console.log(err)
               res.status(500).json({ error: 'Server error' });
@@ -51,6 +71,8 @@ router.get("/ranking", (req, res) => {
 })
 
 router.get("/questions", (req, res) => {
+    // TODO fix this
+
     // pool.query('SELECT  FROM users', (err, res) => {
     //     if(err) {
     //          console.log(err)
@@ -58,6 +80,18 @@ router.get("/questions", (req, res) => {
     //     }
     //     else res.json = res.rows
     // }
+})
+
+router.get("/users", (req, res) => {
+    pool.query('SELECT * FROM Users', (err, query_res) => {
+        if(err) {
+            console.log(err)
+            res.status(500).json({ error: 'Server error' });
+        }
+        else {
+            res.send(query_res.rows);
+        }
+    });
 })
 
 module.exports = router
