b48169425171cc39846cf62047faf17f379536a8
marcelca02 <marcel.calleja@estudiantat.upc.edu>
2023-05-14 01:26:27 +0200

    routing / => login

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { RankingPageComponent } from './components/ranking-page/ranking-page.component';
 import { IndividualTriviaComponent } from './components/individual-trivia/individual-trivia.component';
+import { Router } from '@angular/router';
+import { OnInit } from '@angular/core';
+
 
 const routes: Routes = [
     {path: 'login', component: LoginPageComponent},
@@ -14,4 +17,11 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule implements OnInit{
+    constructor(private router: Router) { }
+
+    ngOnInit() {
+        this.router.navigate(['/login']);
+    }
+
+}
